2ae933efecf39ea3182c5c665669092b2c234484
Wilson E. Alvarez <wilson.e.alvarez@rubonnek.com>
2026-01-20 15:41:42 -0500

    Add support for importing an audio track when injecting subtitles

--- addons/rubonnek.subtitles_importer/plugin.gd
+++ addons/rubonnek.subtitles_importer/plugin.gd
@@ -44,6 +44,9 @@ const TOOL_MENU_ITEM_STRING: String = "Inject Subtitles into AnimationPlayer"
 # The default name used for the generated subtitle animation.
 const ANIMATION_NAME: String = "subtitles"
 
+# Supported audio file extensions
+const AUDIO_EXTENSIONS: Array[String] = ["mp3", "ogg", "wav", "opus"]
+
 
 func _enter_tree() -> void:
 	var script_resource: Resource = get_script()
@@ -88,21 +91,31 @@ func _on_inject_subtitles() -> void:
 	# Get selected resources from FileSystem
 	var selected_paths: PackedStringArray = EditorInterface.get_selected_paths()
 	var subtitles: Subtitles = null
+	var audio_stream: AudioStream = null
 
-	# Find Subtitles from selected files
+	# Find resources from selected files
 	for path: String in selected_paths:
 		var file_extension: String = path.get_extension().to_lower()
+
+		# Check for subtitles
 		if file_extension in Subtitles.supported_extensions:
 			var resource: Resource = load(path)
 			if resource is Subtitles:
 				subtitles = resource
 				print("Found Subtitles: ", path)
-				break
+
+		# Check for audio
+		elif file_extension in AUDIO_EXTENSIONS:
+			var resource: Resource = load(path)
+			if resource is AudioStream:
+				audio_stream = resource
+				print("Found Audio: ", path)
 
 	# Get selected nodes from Scene tree
 	var selected_nodes: Array[Node] = EditorInterface.get_selection().get_selected_nodes()
 	var label_node: Control = null
 	var animation_player: AnimationPlayer = null
+	var audio_stream_player: Node = null
 
 	for node: Node in selected_nodes:
 		if node is Label or node is RichTextLabel:
@@ -111,6 +124,9 @@ func _on_inject_subtitles() -> void:
 		elif node is AnimationPlayer:
 			animation_player = node
 			print("Found AnimationPlayer: ", node.name)
+		elif node is AudioStreamPlayer or node is AudioStreamPlayer2D or node is AudioStreamPlayer3D:
+			audio_stream_player = node
+			print("Found AudioStreamPlayer: ", node.name)
 
 	# Validate selection
 	if subtitles == null:
@@ -128,33 +144,64 @@ func _on_inject_subtitles() -> void:
 		_show_error("No AnimationPlayer node selected in Scene tree!\n\nPlease select an AnimationPlayer node.")
 		return
 
+	# Validate audio selection if provided
+	if audio_stream != null and audio_stream_player == null:
+		printerr("ERROR: Audio file selected but no AudioStreamPlayer node selected!")
+		_show_error("Audio file selected but no AudioStreamPlayer node selected!\n\nPlease select an AudioStreamPlayer, AudioStreamPlayer2D, or AudioStreamPlayer3D node.")
+		return
+
 	# Create the animation
-	var success: bool = _create_subtitle_animation(subtitles, label_node, animation_player)
+	var success: bool = _create_subtitle_animation(
+		subtitles,
+		label_node,
+		animation_player,
+		audio_stream,
+		audio_stream_player
+	)
 
 	if success:
-		print("✓ Successfully created subtitle animation!")
-		print("  Animation name: ", ANIMATION_NAME)
-		print("  Duration: ", subtitles.get_total_duration(), " seconds")
-		print("  Subtitle count: ", subtitles.get_entry_count())
-		print("")
-		print("You can now play the animation in the AnimationPlayer!")
-		print("Note: Make sure the label is visible in your scene to see subtitles.")
+		var duration: float = _calculate_max_duration(subtitles, audio_stream)
 
-		_show_success(
-			"Successfully created subtitle animation!\n\n" +
-			"Animation name: " + ANIMATION_NAME + "\n" +
-			"Duration: " + str(subtitles.get_total_duration()) + " seconds\n" +
-			"Subtitle count: " + str(subtitles.get_entry_count()) + "\n\n" +
-			"You can now play the animation in the AnimationPlayer!\n" +
-			"Note: Make sure the label is visible in your scene to see subtitles.",
-		)
+		var message: String = "✓ Successfully created subtitle animation!\n"
+		message += "  Animation name: " + ANIMATION_NAME + "\n"
+		message += "  Duration: " + str(duration) + " seconds\n"
+		message += "  Subtitle count: " + str(subtitles.get_entry_count()) + "\n"
+
+		if audio_stream != null:
+			message += "  Audio track: Added\n"
+
+		message += "\nYou can now play the animation in the AnimationPlayer!"
+		message += "\nNote: Make sure the label is visible in your scene to see subtitles."
+
+		print(message)
+		_show_success(message)
 	else:
 		printerr("✗ Failed to create subtitle animation.")
 		_show_error("Failed to create subtitle animation.\n\nPlease check the Output console for details.")
 
 
-func _create_subtitle_animation(p_subtitles: Subtitles, p_label_node: Control, p_animation_player: AnimationPlayer) -> bool:
-	# Use Subtitles.inject_animation() to inject the animation
+func _calculate_max_duration(p_subtitles: Subtitles, p_audio_stream: AudioStream) -> float:
+	var max_duration: float = 0.0
+
+	# Get subtitle duration
+	if p_subtitles != null:
+		max_duration = max(max_duration, p_subtitles.get_total_duration())
+
+	# Get audio duration
+	if p_audio_stream != null:
+		max_duration = max(max_duration, p_audio_stream.get_length())
+
+	return max_duration
+
+
+func _create_subtitle_animation(
+	p_subtitles: Subtitles,
+	p_label_node: Control,
+	p_animation_player: AnimationPlayer,
+	p_audio_stream: AudioStream,
+	p_audio_stream_player: Node
+) -> bool:
+	# Use Subtitles.inject_animation() to inject the subtitle animation
 	var result: Error = p_subtitles.inject_animation(ANIMATION_NAME, p_animation_player, p_label_node)
 
 	if result != OK:
@@ -172,8 +219,56 @@ func _create_subtitle_animation(p_subtitles: Subtitles, p_label_node: Control, p
 		_show_error(error_message)
 		return false
 
-	print("Created track:")
+	# Get the animation we just created/modified
+	var animation: Animation = null
+	if p_animation_player.has_animation(ANIMATION_NAME):
+		var library: AnimationLibrary = p_animation_player.get_animation_library("")
+		if library != null:
+			animation = library.get_animation(ANIMATION_NAME)
+
+	if animation == null:
+		printerr("ERROR: Failed to retrieve animation after injection!")
+		return false
+
+	# Calculate maximum duration
+	var max_duration: float = _calculate_max_duration(p_subtitles, p_audio_stream)
+
+	# Update animation length to maximum duration
+	if max_duration > animation.get_length():
+		animation.set_length(max_duration)
+		print("Updated animation length to: ", max_duration, " seconds")
+
+	# Add audio track if audio stream is provided
+	if p_audio_stream != null and p_audio_stream_player != null:
+		var audio_added: bool = _add_audio_track(animation, p_animation_player, p_audio_stream_player, p_audio_stream)
+		if audio_added:
+			print("Added audio track")
+		else:
+			printerr("WARNING: Failed to add audio track")
+
+	print("Created tracks:")
 	print("  - Text track with ", p_subtitles.get_entry_count(), " keyframes")
+	if p_audio_stream != null:
+		print("  - Audio track")
+
+	return true
+
+
+func _add_audio_track(p_animation: Animation, p_animation_player: AnimationPlayer, p_audio_player: Node, p_audio_stream: AudioStream) -> bool:
+	# Get the node path from AnimationPlayer to AudioStreamPlayer
+	var node_path: NodePath = p_animation_player.get_node(p_animation_player.get_root_node()).get_path_to(p_audio_player)
+
+	# Remove existing audio tracks for this player if any
+	for i: int in range(p_animation.get_track_count() - 1, -1, -1):
+		if p_animation.track_get_type(i) == Animation.TYPE_AUDIO:
+			var track_path: String = str(p_animation.track_get_path(i))
+			if track_path == str(node_path):
+				p_animation.remove_track(i)
+
+	# Add audio track
+	var audio_track_idx: int = p_animation.add_track(Animation.TYPE_AUDIO)
+	p_animation.track_set_path(audio_track_idx, node_path)
+	p_animation.audio_track_insert_key(audio_track_idx, 0.0, p_audio_stream)
 
 	return true
 
